a6dcf250f2c8478eb6b1475f6f36fba5947351e3
Viraj Kabbur <viraj.kabbur@reallivesworld.com>
2026-03-26 15:45:22 +0530

    Faster loading images

--- Changemaker-web/src/Component/Pages/Certifiedinstitutes.jsx
+++ Changemaker-web/src/Component/Pages/Certifiedinstitutes.jsx
@@ -312,7 +312,7 @@ const Certifiedinstitutes = ({ theme, settheme }) => {
           <img
             loading="lazy"
             decoding="async"
-            src="https://reallivesfrontend.s3.us-east-1.amazonaws.com/certificate.svg"
+            src="https://d2jn82ki4w4ftn.cloudfront.net/certificate.svg"
             alt="Changemaker Index image"
           />
         </div>
